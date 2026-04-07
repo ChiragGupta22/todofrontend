8739f03bf42d8777255f975cf45264abd973a7a8
Chirag <chiragguptaworks@gmail.com>
2026-04-07 12:34:52 +0530

    yes done

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    "https://backend1-1-f2a5.onrender.com/api " || "http://localhost:3000/api",
+  baseURL: "https://backend1-1-f2a5.onrender.com/api",
   withCredentials: true,
 });
