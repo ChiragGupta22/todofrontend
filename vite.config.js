5c2d1c9157155dd195aee33a38b0f1ebb9517db3
Chirag <chiragguptaworks@gmail.com>
2026-04-07 12:07:06 +0530

    yes done

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://backend1-1-f2a5.onrender.com",
-        changeOrigin: true,
-        secure: false, // For HTTPS self-signed/Render
-        configure: (proxy, options) => {
-          proxy.on("proxyReq", (proxyReq, req, res) => {
-            proxyReq.setHeader(
-              "Origin",
-              "https://frontend1-ecru-zeta.vercel.app",
-            );
-          });
-        },
-      },
-    },
-  },
 });
