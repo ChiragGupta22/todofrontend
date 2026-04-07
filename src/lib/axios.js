3de6bc835ab36dbc5a792c5cc1034dcbdd92daf7
Chirag <chiragguptaworks@gmail.com>
2026-04-07 13:11:56 +0530

    yes done

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
+  baseURL: "https://backend1-1-f2a5.onrender.com/api",
   withCredentials: true,
 });
--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://backend1-1-f2a5.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        credentials: "include",
-      },
-    },
-  },
 });
