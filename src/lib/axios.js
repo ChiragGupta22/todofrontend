e3388e6d3002b93c1c21d8e21ddf0f14b86ae93a
Chirag <chiragguptaworks@gmail.com>
2026-04-08 23:32:42 +0530

    yes done

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://todobackend-1-phu8.onrender.com/api",
-  withCredentials: true,
-});
+const api = axios
+  .create({
+    baseURL: "https://todobackend-1-phu8.onrender.com/api",
+    withCredentials: true,
+  })
+  .then((res) => {
+    console.log("USER:", res.data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 export default api;
